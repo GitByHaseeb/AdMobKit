3500558cf15cb161d20f56f59e9edcad383ab5c1
AbdulHaseeb <Abdul.Haseeb@o3interfaces.com>
2026-01-05 18:27:20 +0500

    SPM setup for AdMobKit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+import PackageDescription
+
+let package = Package(
+    name: "AdMobKit",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "AdMobKit",
+            targets: ["AdMobKit"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "10.0.0")
+    ],
+    targets: [
+        .target(
+            name: "AdMobKit",
+            dependencies: [
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+            ],
+            path: "Sources/AdMobKit",
+            publicHeadersPath: "Sources/AdMobKit"
+        ),
+        .testTarget(
+            name: "AdMobKitTests",
+            dependencies: ["AdMobKit"],
+            path: "Tests/AdMobKitTests"
+        )
+    ]
+)
+
